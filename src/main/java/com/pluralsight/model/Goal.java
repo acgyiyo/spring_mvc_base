73f1d07a92320fec45fe56c57789c4a95b769ba8
acgyiyo <acgyiyo1@gmail.com>
2019-04-22 17:08:20 -0500

    se configuran goal y exercise para que tengan relacion, sean entidades que persistan en la BD y se crean sus metodos en el repositorio, controller y servicio

--- src/main/java/com/pluralsight/model/Goal.java
+++ src/main/java/com/pluralsight/model/Goal.java
@@ -1,13 +1,40 @@
 package com.pluralsight.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
 import org.hibernate.validator.constraints.Range;
 
-
-
+@Entity
+@Table(name = "goals")
 public class Goal {
 
+	@Id
+	@GeneratedValue
+	@Column(name = "GOAL_ID")
+	private Long id;
+
 	@Range(min = 1, max = 120)
+	@Column(name = "MINUTES")
 	private int minutes;
+	
+	@OneToMany
+	private List<Exercise> exercises = new ArrayList<>();
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
 
 	public int getMinutes() {
 		return minutes;
@@ -16,5 +43,5 @@ public class Goal {
 	public void setMinutes(int minutes) {
 		this.minutes = minutes;
 	}
-	
+
 }
